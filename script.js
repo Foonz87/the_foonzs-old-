710600466a165d0279a613e722dcedd6ed17dbb3
Paul Thoonen <paul.thoonen@gmail.com>
2017-06-10 03:07:55 +1000

    Working on Images

--- script.js
+++ script.js
@@ -1,29 +1,47 @@
+var aboutCharlie = "This is Charlie."
+var aboutAtticus = "This is Atticus."
+var aboutSamara = "This is Samara."
+var aboutBrooklyn = "This is Brooklyn."
+var aboutBellatrix = "This is Bellatrix."
+var aboutProngs = "This is Prongs."
+
 function imageCharlie()
   {
-  document.getElementById('image').src="images/charlie.jpg";
+    document.getElementById('image').src="images/charlie.jpg";
+    $('.aboutPets').text(aboutCharlie);
   }
 
 function imageAtticus()
   {
-  document.getElementById('image').src="images/atticus.jpg";
+    document.getElementById('image').src="images/atticus.jpg";
+    $('.aboutPets').text(aboutAtticus);
   }
 
 function imageSamara()
   {
-  document.getElementById('image').src="images/samara.jpg";
+    document.getElementById('image').src="images/samara.jpg";
+    $('.aboutPets').text(aboutSamara);
   }
 
 function imageBrooklyn()
   {
-  document.getElementById('image').src="images/brooklyn.jpg";
+    document.getElementById('image').src="images/brooklyn.jpg";
+    $('.aboutPets').text(aboutBrooklyn);
   }
 
 function imageBellatrix()
   {
-  document.getElementById('image').src="images/bellatrix.jpg";
+    document.getElementById('image').src="images/bellatrix.jpg";
+    $('.aboutPets').text(aboutBellatrix);
   }
 
 function imageProngs()
   {
-  document.getElementById('image').src="images/prongs.jpg";
+    document.getElementById('image').src="images/prongs.jpg";
+    $('.aboutPets').text(aboutProngs);
+  }
+
+function imageAbout()
+  {
+    $('.image').click($('.aboutPets').toggle());
   }
